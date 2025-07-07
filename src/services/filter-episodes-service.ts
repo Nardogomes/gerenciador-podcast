096a9dc0505792d34ab9f2a7856fba70c2ec7247
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-07-06 23:43:18 -0300

    feat: implement status code error handling.

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,10 +1,21 @@
 import { IncomingMessage } from "node:http"
 import { repositoryPodcast } from "../repositories/podcasts-repository"
+import { PodcastTransferModel } from "../models/podcast-transfer-model"
+import { StatusCode } from "../utils/status-code"
 
-export const serviceFilterEpisodes = async (podcastName: IncomingMessage) => {
+export const serviceFilterEpisodes = async (podcastName: IncomingMessage):Promise<PodcastTransferModel> => {
+  let responseFormat: PodcastTransferModel = {
+    statusCode: 0,
+    body: []
+  }
+  
   const queryString = podcastName.url?.split("?p=")[1] || ""
 
   const data = await repositoryPodcast(queryString)
+
+  responseFormat.statusCode = data.length !== 0 ? StatusCode.OK : StatusCode.NoContent
+
+  responseFormat.body = data
   
-  return data
+  return responseFormat
 }
--- src/utils/status-code.ts
+++ src/utils/status-code.ts
@@ -1,3 +1,6 @@
 export enum StatusCode {
   OK = 200,
+  Created = 201,
+  Accepted = 202,
+  NoContent = 204
 }
